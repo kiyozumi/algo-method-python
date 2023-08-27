ee519a57ee7d2fe654f1d934a1844f1fdaadb6f0
kiyozumi <kiyozumi.obuchi@gmail.com>
2023-08-27 12:37:19 +0900

    Add numeric_3 file

---
+++ numeric/numeric_3.py
@@ -0,0 +1,13 @@
+# https://algo-method.com/tasks/222
+N = int(input())
+
+if N == 1:
+    print('No')
+    exit()
+
+for x in range(2, N):
+    if N % x == 0:
+        print("No")
+        exit()
+
+print("Yes")
