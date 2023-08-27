d051c73645c54efd5316c216fb4ae56299855536
kiyozumi <kiyozumi.obuchi@gmail.com>
2023-08-27 17:16:11 +0900

    Add numeric_5 file

---
+++ numeric/numeric_5.py
@@ -0,0 +1,13 @@
+# https://algo-method.com/tasks/225
+
+N = int(input())
+
+for i in range(1, N + 1):
+    if i % 3 == 0 and i % 5 == 0:
+        print("FizzBuzz")
+    elif i % 3 == 0:
+        print("Fizz")
+    elif i % 5 == 0:
+        print("Buzz")
+    else:
+        print(i)
